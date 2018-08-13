a16e13e9f03ad48ea215f6d080553fb21b3fa688
Michael Samy <mike@composedhealth.com>
2018-08-13 16:10:26 +0100

    work on avatars on about page

--- themes/storygen/js/scripts.js
+++ themes/storygen/js/scripts.js
@@ -1,26 +1,47 @@
 jQuery(document).ready(function($) {
-  			// grab the initial top offset of the navigation
-  		   	var stickyNavTop = $('.sg-nav-container').offset().top;
-          var navHeight = $('.sg-nav-container').height();
+  // grab the initial top offset of the navigation
+  var stickyNavTop = $('.sg-nav-container').offset().top;
+  var navHeight = $('.sg-nav-container').height();
 
-  		   	// our function that decides weather the navigation bar should have "fixed" css position or not.
-  		   	var stickyNav = function(){
-  			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+  // our function that decides weather the navigation bar should have "fixed" css position or not.
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 
-  			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-  			    // otherwise change it back to relative
-  			    if (scrollTop > stickyNavTop) {
-  			        $('.sg-nav-container').addClass('sticky');
-                $('.layout-container').css('margin-top',navHeight);
-  			    } else {
-  			        $('.sg-nav-container').removeClass('sticky');
-                $('.layout-container').css('margin-top',0);
-  			    }
-  			};
+    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+    // otherwise change it back to relative
+    if (scrollTop > stickyNavTop) {
+      $('.sg-nav-container').addClass('sticky');
+      $('.layout-container').css('margin-top',navHeight);
+    } else {
+      $('.sg-nav-container').removeClass('sticky');
+      $('.layout-container').css('margin-top',0);
+    }
+  };
 
-  			stickyNav();
-  			// and run it again every time you scroll
-  			$(window).scroll(function() {
-  				stickyNav();
-  			});
+  stickyNav();
+  // and run it again every time you scroll
+  $(window).scroll(function() {
+    stickyNav();
+  });
+
+
+
+})
+
+
+
+
+jQuery(document).ready(function($) {
+
+  $('.sg-about-icon-avatar').click(function(event) {
+    var tid = event.target.id;
+    var offset = $('#team-member-'+tid).offset();
+    $("html, body").animate({
+      scrollTop: offset.top-145
+    }, {
+    duration: 1000
+    });
+    console.log(offset.top);
+  });
+
 })
